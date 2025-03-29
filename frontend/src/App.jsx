e2029489443906edbc3fad14a30f28d8ac73f1e3
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2025-03-28 22:10:47 -0400

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+   <>
+   
+    <Text display= "Hello World"/ >
+    <Text display= "Juan Sucks Ass"/ >
+   </>
+  )
+}
+
+function Text({display}){
+  return(
+    <div>
+      <p>{display}</p>
+    </div>
+  );
+}
+
+export default App
