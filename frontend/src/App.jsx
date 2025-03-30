41dc602e908b832a9bda5dd070e040527220572f
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2025-03-30 17:08:53 -0400

    feat conditional rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,15 @@ import './App.css'
 import MovieCard from './components/MovieCard'
 function App() {
 
-
+  const movieNumber = 2;
   return (
+
    <>
-     <MovieCard movie = {{title: "Juan's Movie", release_date: "2024", }} />
-     <MovieCard movie = {{title: "pleb's Movie", release_date: "2025", }} />
+    {movieNumber === 1 ? (
+      <MovieCard movie = {{title: "Juan's Movie", release_date: "2024", }} />
+    ) : (
+      <MovieCard movie = {{title: "pleb's Movie", release_date: "2025", }} />
+    )}
    </>
   )
 }
